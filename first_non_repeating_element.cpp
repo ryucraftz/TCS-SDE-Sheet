08160c83984665960d9728e7994accc5b958fd3b
ryucraftz <vaibhavsathe0204gmail.com>
2025-09-03 21:03:08 +0530

    j is declrared properly

--- first_non_repeating_element.cpp
+++ first_non_repeating_element.cpp
@@ -1,19 +1,16 @@
-class Solution{
-    public:
-    vector<int> firstNonRepeating(vector<int>& arr){
-        int n = arr.size();
-        for (int i = 0; i < n; i++) {
-            int j;
-            for (j = 0; j < n; j++) {
-                if (i != j && arr[i] == arr[j]) {
-                    break;
-                }
-            }
-            if (j == n) {
-                return {arr[i]};
-            }
-        }
-        return {-1};
-
+class Solution {
+  public:
+    int firstNonRepeating(vector<int>& arr) {
+      int n = arr.size();
+      
+      for (int i = 0; i < n; i++){
+        int j;
+          for ( j = 0; j < n; j++){
+          if (i!=j && arr[i]==arr[j])
+          break;}
+          if (j==n)
+          return arr[i];
+      }
+      return -1;
     }
-}
+};
