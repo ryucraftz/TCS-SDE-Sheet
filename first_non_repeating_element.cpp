fbc76b0da9837b4230ddd1d09cd23eea1465d5e9
ryucraftz <vaibhavsathe0204gmail.com>
2025-09-03 20:50:15 +0530

    question1

---
+++ first_non_repeating_element.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+    vector<int> firstNonRepeating(vector<int>& arr){
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
+            int j;
+            for (j = 0; j < n; j++) {
+                if (i != j && arr[i] == arr[j]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return {arr[i]};
+            }
+        }
+        return {-1};
+
+    }
+}
